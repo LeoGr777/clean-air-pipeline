86068079e9acd9aa5c58735c94530e0b22cc8482
Leo Grund <leogrund10@gmail.com>
2025-07-29 19:36:01 +0200

    feauture(sensors): started to implement sensor extraction task, not finished yet

---
+++ src/extract_openaq_sensors.py
@@ -0,0 +1,71 @@
+"""
+Extracts sensor endpoint data for a given list of locations from openaq API and uploads it to S3.
+"""
+
+# ### IMPORTS ###
+
+# 1.1 Standard Libraries
+import os
+import logging
+
+# 1.2 Third-party libraries
+from dotenv import load_dotenv
+import boto3
+
+# 1.3 Local application modules
+from utils.extract_openaq_utils import fetch_all_pages, upload_to_s3
+
+
+# =============================================================================
+# 2. CONSTANTS AND GLOBAL SETTINGS
+# =============================================================================
+load_dotenv()
+
+
+# S3 specific constants
+S3_BUCKET = os.getenv("S3_BUCKET")
+RAW_S3_ENDPOINT_LOCATIONS = "raw/sensors"
+
+
+# Configure root logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - [%(levelname)s] - %(message)s"
+)
+
+# The S3 client is initialized once
+s3 = boto3.client("s3")
+
+# =============================================================================
+# 3. MAIN LOGIC
+# =============================================================================
+def main():
+    """
+    Orchestrates the fetching of sensors and uploading the result to S3.
+    """
+    logging.info(f"Starting sensors extraction for locations: {COUNTRY}")
+
+    # Step 1: Fetch all sensors using the utility function
+    sensors = fetch_all_pages(endpoint="sensors", params={"iso": COUNTRY})
+
+    # Step 2: If data was fetched, upload it to S3
+    if locations:
+        logging.info(f"Successfully fetched {len(locations)} locations.")
+
+        # Call the generic S3 upload utility function
+        upload_to_s3(
+            s3_client=s3,
+            bucket_name=S3_BUCKET,
+            endpoint=RAW_S3_ENDPOINT_LOCATIONS,
+            data=locations
+        )
+    else:
+        logging.warning("No locations were fetched. Nothing to upload.")
+
+    logging.info("Locations extraction task finished.")
+
+# =============================================================================
+# 4. SCRIPT EXECUTION
+# =============================================================================
+if __name__ == "__main__":
+    main()
