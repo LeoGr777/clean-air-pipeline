710e0b0ed0d8e39a90cfd516155e36f2564917e7
Leo Grund <leogrund10@gmail.com>
2025-08-25 12:11:41 +0200

    feat: adjusting extracin logic to one file per endpoint request

--- src/extract_openaq_sensors.py
+++ src/extract_openaq_sensors.py
@@ -11,12 +11,13 @@ import logging
 # 1.2 Third-party libraries
 from dotenv import load_dotenv
 import boto3
+from pathlib import Path 
 
 dotenv_path = Path(__file__).parent.parent / '.env'
 load_dotenv(dotenv_path=dotenv_path)
 
 # 1.3 Local application modules
-from utils.extract_openaq_utils import fetch_all_pages, upload_to_s3, fetch_sequentially, find_latest_s3_key, read_json_from_s3
+from utils.extract_openaq_utils import fetch_all_pages_new, get_yesterday_iso_date, upload_to_s3, find_latest_s3_key, read_json_from_s3
 
 
 # =============================================================================
@@ -24,9 +25,10 @@ from utils.extract_openaq_utils import fetch_all_pages, upload_to_s3, fetch_sequ
 # =============================================================================
 # S3 specific constants
 S3_BUCKET = os.getenv("S3_BUCKET")
-RAW_S3_ENDPOINT_SENSORS = "raw/sensors"
-BASE_URL = "https://api.openaq.org/v3/sensors"
+RAW_S3_ENDPOINT = "raw/sensors"
+BASE_URL = "https://api.openaq.org/v3"
 PROCESSED_LOCATIONS_PREFIX = "processed/dim_location"
+ENDPOINT = "sensors"
 
 
 # Configure root logger
@@ -47,6 +49,9 @@ def main():
     """
     logging.info(f"Starting sensors extraction task")
 
+    yesterday_iso_string = get_yesterday_iso_date()
+    URL_PARAMS = {"datetime_from": yesterday_iso_string}
+
     # Dynamically find the latest location_ids file
     location_key = find_latest_s3_key(
         s3_client=s3,
@@ -56,44 +61,59 @@ def main():
     )
 
     if not location_key:
-        logging.error("Could not key for location list. Aborting.")
+        logging.error("Could not find sensor_ids from location task. Aborting.")
         return
     
-    location_ids = read_json_from_s3(
+    sensor_ids = read_json_from_s3(
     s3_client=s3, 
     bucket_name=S3_BUCKET,
     s3_key=location_key
     )
 
-    # Build a list of all URLs to fetch
-    urls_to_fetch = [f"{BASE_URL}/{loc_id}" for loc_id in location_ids]
-    print(urls_to_fetch)
-    
-    logging.info(f"Prepared {len(urls_to_fetch)} URLs for sensor data fetching.")
+    num_urls = len(sensor_ids)
+    logging.info(f"Prepared {num_urls} URLs for sensor data fetching.")
 
-    # Fetch all data sequentially
-    sensor_data = fetch_sequentially(urls=urls_to_fetch, requests_per_minute=55)
+    # Logging progress counter for sensor_ids
+    i = 1
 
-    # Instead of uploading the raw sensor_data, create a clean list
-    all_sensor_records = []
+    fetched_urls = []
 
-    for response in sensor_data:
-        # Check if the response was successful and has a 'results' key
-        if response and "results" in response:
-            all_sensor_records.extend(response["results"])
+     # Loop through each sensor_id one by one
+    for sensor_id in sensor_ids:
+        try:
+            # Build the specific URL for this ID
+            url = f"{BASE_URL}/{ENDPOINT}/{sensor_id}"
+            endpoint = f"{ENDPOINT}/{sensor_id}"
+            logging.info(f"{i}/{num_urls} Fetching data for sensor {sensor_id} from {url} with params: {URL_PARAMS}")
 
-    # Upload the combined results to S3
-    if sensor_data:
-        upload_to_s3(
-            s3_client=s3,
-            bucket_name=S3_BUCKET,
-            endpoint=RAW_S3_ENDPOINT_SENSORS,
-            data=all_sensor_records,
-        )
-    else:
-        logging.warning("No sensor data was fetched.")
-    
-    logging.info("Sensor extraction task finished.")
+            # Build file_prefix
+            resource = RAW_S3_ENDPOINT.split("/")[1]
+            file_prefix = f"{resource}_sensor_{sensor_id}"
+
+            # Fetch data, one url at a time
+            response_data = fetch_all_pages_new(endpoint, URL_PARAMS)
+
+            if response_data[0]["results"]:
+                fetched_urls.append(f"{url}?{URL_PARAMS}")
+
+            i += 1
+
+            # Upload results per endpoint to S3
+            if response_data:
+                upload_to_s3(
+                    s3_client=s3,
+                    bucket_name=S3_BUCKET,
+                    endpoint=RAW_S3_ENDPOINT,
+                    data=response_data,
+                    file_prefix=file_prefix,
+                )
+            else:
+                logging.warning("No sensor data was fetched.")
+        except Exception as e:
+            logging.exception("Error executing meausurement extract task")
+
+    logging.info(f"Finished extract task for sensor for {i}/{num_urls} urls")
+    exit()
 
 # =============================================================================
 # 4. SCRIPT EXECUTION
