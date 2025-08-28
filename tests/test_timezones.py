572fee65c07c1fa6312f18f2d5aa374de9b26044
Leo Grund <leogrund10@gmail.com>
2025-08-28 23:22:50 +0200

    chore: removed .vscode and /test files from repo

--- tests/test_timezones.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import datetime as dt
-
-# Simuliere deine DataFrame-Erstellung
-df = pd.DataFrame()
-df["ingest_ts"] = [dt.datetime.now(dt.timezone.utc)]
-
-print(f"DataFrame value: {df['ingest_ts'].iloc[0]}")
-print(f"DataFrame dtype: {df['ingest_ts'].dtype}")
