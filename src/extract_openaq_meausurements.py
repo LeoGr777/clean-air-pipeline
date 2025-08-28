36218b939af07f7f8c28d9d18d9390aacc8b4c72
Leo Grund <leogrund10@gmail.com>
2025-08-28 18:39:25 +0200

    feat: adjusted code so it is similar to other extract tasks

--- src/extract_openaq_meausurements.py
+++ src/extract_openaq_meausurements.py
@@ -9,6 +9,7 @@ import os
 import logging
 import datetime as dt
 from pathlib import Path 
+import pandas as pd
 
 # 1.2 Third-party libraries
 from dotenv import load_dotenv
@@ -18,13 +19,11 @@ dotenv_path = Path(__file__).parent.parent / '.env'
 load_dotenv(dotenv_path=dotenv_path)
 
 # 1.3 Local application modules
-from utils.extract_openaq_utils import upload_to_s3, fetch_all_pages_new, find_latest_s3_key, read_json_from_s3
+from utils.extract_openaq_utils import upload_to_s3, fetch_all_pages_new, find_latest_s3_key, read_json_from_s3, get_yesterday_iso_date
 
 # =============================================================================
 # 2. CONSTANTS AND GLOBAL SETTINGS
 # =============================================================================
-
-API_KEY = os.getenv("API_KEY")
 
 # S3 specific constants
 S3_BUCKET = os.getenv("S3_BUCKET")
@@ -33,7 +32,7 @@ BASE_URL = "https://api.openaq.org/v3"
 EDNDPOINT = "sensors"
 MEASUREMENT_SUFFIX = "hours"
 PROCESSED_SENSORS_PREFIX = "processed/dim_sensor"
-SENSOR_LIST_FILE_PATTERN = "sensor_id_list"
+SENSOR_LIST_FILE_PATTERN = "sensors"
 
 # Configure root logger
 logging.basicConfig(
@@ -53,13 +52,8 @@ def main():
     """
     logging.info(f"Starting measurements (hourly) extraction task")
 
-    # Calculate yesterday at midnight in UTC
-    yesterday_utc = dt.datetime.now(dt.timezone.utc) - dt.timedelta(days=1)
-    yesterday_midnight_utc = yesterday_utc.replace(hour=0, minute=0, second=0, microsecond=0)
-
-    # Format into the required string
-    yesterday_iso_string = yesterday_midnight_utc.isoformat().replace('+00:00', 'Z')
-    URL_PARAMS = {"datetime_from": yesterday_iso_string}
+    # Fetch only data from yesterday
+    URL_PARAMS = {"datetime_from": get_yesterday_iso_date()}
 
     # Dynamically find the latest sensor_id file to generate endpoint urls later
     sensor_key = find_latest_s3_key(
@@ -73,26 +67,27 @@ def main():
         logging.error("Could not find key for sensor list. Aborting.")
         return
     
-    # Read sensor_id file
-    sensor_ids = read_json_from_s3(
-    s3_client=s3, 
-    bucket_name=S3_BUCKET,
-    s3_key=sensor_key
-    )
+    s3_uri = f"s3://{S3_BUCKET}/{sensor_key}"
 
+    # Read the file directly from S3 into a DataFrame
+    print(f"Reading file from {s3_uri}...")
+    
+    df = pd.read_parquet(s3_uri)
+    sensor_id_list = df["openaq_sensor_id"].to_list()
+ 
     logging.info("Starting paginated, sequential data fetching...")
 
-    num_urls = len(sensor_ids)
+    num_urls = len(sensor_id_list)
 
     logging.info(f"{num_urls} URLs to fetch")
 
-    # Logging progress counter for sensor_ids
+    # Logging progress counter for sensor_id_list
     i = 1
 
     fetched_urls = []
 
      # Loop through each sensor ID one by one
-    for sensor_id in sensor_ids:
+    for sensor_id in sensor_id_list:
         try:
             # Build the specific URL for this ID
             url = f"{BASE_URL}/{EDNDPOINT}/{sensor_id}/{MEASUREMENT_SUFFIX}"
@@ -109,7 +104,7 @@ def main():
             if response_data[0]["results"]:
                 fetched_urls.append(f"{url}?{URL_PARAMS}")
             
-            i +=1
+            i += 1
 
             # Upload results per endpoint to S3
             if response_data:
