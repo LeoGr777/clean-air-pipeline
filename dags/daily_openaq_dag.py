17b6e79574d7906e7fbc9f2a2a7f0d2e6a04852e
Leo Grund <leogrund10@gmail.com>
2025-08-31 15:33:39 +0200

    feat: added daily and weekly airflow DAGS

---
+++ dags/daily_openaq_dag.py
@@ -0,0 +1,30 @@
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+
+# Import the specific runner function for the daily tasks
+from src.pipeline_runner import run_daily_pipeline
+
+with DAG(
+    dag_id='daily_clean_air_etl',
+    start_date=pendulum.datetime(2025, 8, 30, tz="Europe/Berlin"),
+    schedule='@daily',
+    catchup=False,
+    doc_md="""
+    ### Daily Clean Air ETL DAG
+
+    This DAG runs daily to perform the following steps:
+    - Extracts data from the OpenAQ API for endpoints: locations, sensors, measurements
+    - Stores data on S3 raw/
+    - Transforms the raw data into a clean, usable format
+    - Stores data on S3 /processed
+    - Loads the transformed data into Snowflake dim and fact tables
+    """,
+    tags=['daily', 'etl', 'openaq'],
+) as dag:
+    
+    run_daily_etl_task = PythonOperator(
+        task_id='run_daily_etl',
+        python_callable=run_daily_pipeline
+    )
